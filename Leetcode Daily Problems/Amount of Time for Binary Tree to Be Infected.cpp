5745fe835e45259e9de56bfbb7c4aa70bb24a743
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-01-10 14:32:13 +0530

    Leetcode Daily on 10 Jan 2024

--- Leetcode Daily Problems/Amount of Time for Binary Tree to Be Infected.cpp
+++ Leetcode Daily Problems/Amount of Time for Binary Tree to Be Infected.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 /*
-Approach -- 
+Approach -- Store the parents of each node in a map & then we find do a bfs assignParents from the start node
+In bfs the the level of last node visited is the time to spread the infection
 */
 
 class TreeNode
@@ -20,17 +21,108 @@ public:
     }
 };
 
+/*
+Approach -- Store the parents of each node in a map & then we find do a bfs assignParents from the start node
+In bfs the the level of last node visited is the time to spread the infection
+*/
+
 class Solution
 {
+protected:
+    void assignParents(TreeNode *node, TreeNode *parent, unordered_map<TreeNode *, TreeNode *> &par, TreeNode *&source, int start)
+    {
+        if (node == NULL)
+        {
+            return;
+        }
+
+        if (node->val == start)
+        {
+            source = node;
+        }
+
+        par[node] = parent;
+        assignParents(node->left, node, par, source, start);
+        assignParents(node->right, node, par, source, start);
+    }
+
 public:
     int amountOfTime(TreeNode *root, int start)
     {
+        TreeNode *source;
+        int minTime = 0;
+        const int INF = 1e9 + 10;
+        unordered_map<TreeNode *, int> time;
+        unordered_map<TreeNode *, TreeNode *> par;
 
+        assignParents(root, NULL, par, source, start);
+
+        for (auto node : par)
+        {
+            time[node.first] = INF;
+        }
+
+        queue<TreeNode *> q;
+        q.push(source);
+        time[source] = 0;
+
+        while (!q.empty())
+        {
+            TreeNode *curr = q.front();
+            q.pop();
+
+            if (curr->left != NULL)
+            {
+                TreeNode *node = curr->left;
+
+                int prevTime = time[node];
+                int newTime = time[curr] + 1;
+
+                if (newTime < prevTime)
+                {
+                    time[node] = newTime;
+                    minTime = max(minTime, newTime);
+                    q.push(node);
+                }
+            }
+
+            if (curr->right != NULL)
+            {
+                TreeNode *node = curr->right;
+
+                int prevTime = time[node];
+                int newTime = time[curr] + 1;
+
+                if (newTime < prevTime)
+                {
+                    time[node] = newTime;
+                    minTime = max(minTime, newTime);
+                    q.push(node);
+                }
+            }
+
+            if (par[curr] != NULL)
+            {
+                TreeNode *node = par[curr];
+
+                int prevTime = time[node];
+                int newTime = time[curr] + 1;
+
+                if (newTime < prevTime)
+                {
+                    time[node] = newTime;
+                    minTime = max(minTime, newTime);
+                    q.push(node);
+                }
+            }
+        }
+
+        return minTime;
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(N + N) = O(N) -- traversal for storing parents & bfs
+// SC : O(N + N) = O(N) -- store parents & time
 
 int main()
 {
--- Leetcode Daily Problems/Arithmetic Slices II - Subsequence.cpp
+++ Leetcode Daily Problems/Arithmetic Slices II - Subsequence.cpp
@@ -2,14 +2,51 @@
 using namespace std;
 
 /*
-Approach --
+Approach -- It is a dp problem in this dp the tabulation is way easy then
 */
 
 class Solution
 {
+protected:
+    // memoisation
+    int numberOfAp(int ind, long long diff, vector<int> &nums, vector<pair<int, long long>> &numAp)
+    {
+        int n = nums.size();
+
+        int firstTerm = nums[ind];
+        long long nextTerm = firstTerm + 0LL + diff;
+
+        int totalApSequences = 0;
+
+        for (int i = ind + 1; i < n; i++)
+        {
+            long long num = nums[i];
+
+            if (num == nextTerm)
+            {
+                totalApSequences += numberOfAp(i, diff, nums, numAp) + 1;
+            }
+        }
+
+        return totalApSequences;
+    }
+
 public:
     int numberOfArithmeticSlices(vector<int> &nums)
     {
+        int n = nums.size(), totalApSequences = 0;
+        vector<pair<int, long long>> numAp;
+
+        for (int ind1 = 0; ind1 < n; ind1++)
+        {
+            for (int ind2 = ind1 + 1; ind2 < n; ind2++)
+            {
+                long long diff = nums[ind2] + 0LL - nums[ind1];
+                totalApSequences += numberOfAp(ind2, diff, nums, numAp);
+            }
+        }
+
+        return totalApSequences;
     }
 };
 
