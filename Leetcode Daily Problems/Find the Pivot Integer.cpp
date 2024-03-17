b83176727213994d6d67d46ccf859a6246e1a526
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-03-18 02:36:25 +0530

    Leetcode Problems

--- Leetcode Daily Problems/Find the Pivot Integer.cpp
+++ Leetcode Daily Problems/Find the Pivot Integer.cpp
@@ -2,19 +2,114 @@
 using namespace std;
 
 /*
-Approach --
+Approach -- We can use many approaches in this problem like simple maths equation which is x^2 = (n*(n+1))/2 .
+We can also use two pointers to get the sum from both ends & increase it until they become equal .
+We can also use binary search in this problem as we find if pivot^2 == (n) * (n+1)/2 . If it is equal then it
+is our pivot number but if pivot square is less then we will increase lo to mid+1 else hi to mid - 1 .
 */
 
+// Maths Approach
 class Solution
 {
 public:
     int pivotInteger(int n)
     {
+        int totalSum = ((n * (n + 1)) / 2);
+        int pivot = sqrt(totalSum);
+
+        return ((pivot * pivot == totalSum) ? pivot : -1);
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(1)
+// SC : O(1)
+
+// Two Pointers Approach
+class Solution
+{
+public:
+    int pivotInteger(int n)
+    {
+        int pivotNumber = -1;
+        int leftNum = 1, rightNum = n;
+        int leftSum = 1, rightSum = n;
+
+        while (leftNum <= rightNum)
+        {
+            int leftAndRightSumComparison = (leftSum >= rightSum) + (leftSum == rightSum);
+
+            switch (leftAndRightSumComparison)
+            {
+            case 0:
+                leftNum = leftNum + 1;
+                leftSum = leftSum + leftNum;
+                break;
+
+            case 1:
+                rightNum = rightNum - 1;
+                rightSum = rightSum + rightNum;
+                break;
+
+            case 2:
+                if (leftNum == rightNum)
+                {
+                    pivotNumber = leftNum;
+                    return pivotNumber;
+                }
+
+                leftNum = leftNum + 1;
+                leftSum = leftSum + leftNum;
+
+                rightNum = rightNum - 1;
+                rightSum = rightSum + rightNum;
+            }
+        }
+
+        return pivotNumber;
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
+
+// Binary Search Approach
+class Solution
+{
+public:
+    int pivotInteger(int n)
+    {
+        int totalSum = ((n * (n + 1)) / 2);
+        int lo = 1, hi = n, pivotNumber = -1;
+
+        while (lo <= hi)
+        {
+            int mid = (lo + hi) >> 1;
+            int pivotSquare = mid * mid;
+
+            int pivotAndTotalSumComparison = (pivotSquare >= totalSum) + (pivotSquare == totalSum);
+
+            switch (pivotAndTotalSumComparison)
+            {
+            case 0:
+                lo = mid + 1;
+                break;
+
+            case 1:
+                hi = mid - 1;
+                break;
+
+            case 2:
+                pivotNumber = mid;
+                return pivotNumber;
+            }
+        }
+
+        return pivotNumber;
+    }
+};
+
+// TC : O(log(N))
+// SC : O(1)
 
 int main()
 {
--- NeetCode Roadmap & All/Intervals/Insert Interval.cpp
+++ NeetCode Roadmap & All/Intervals/Insert Interval.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 /*
-Approach --
+Approach 1 -- As the start & end time are sorted we can find the left & right Portion of the intervals which is
+safe from newInterval . And in case of the portion which is going to merge with interval we can find the lower
+& upper values & then merge it . Finally append all in the answers array .
+
+Approach 2 -- We can traverse the intervals & find if the current interval is less then newInterval then append
+it in the answer if we find the interval which is going to merge with newInterval , we merged them & then update
+the newInterval as merged interval continue , until we get and interval which is greater than newInterval . We
+break the loop and append the updated newInterval & all other intervals greater than it in answer array .
 */
 
+// Approach 1
 class Solution
 {
 protected:
@@ -132,13 +140,86 @@ public:
         {
             finalIntervals.push_back(intervals[ind]);
         }
-        
+
         return finalIntervals;
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(N * log(N))
+// SC : O(N)
+
+// Approach 2
+class Solution
+{
+protected:
+    int isIntervalSmallerGreaterOrCanBeMerged(vector<int> &interval, vector<int> &newInterval)
+    {
+        int currStart = interval[0];
+        int currEnd = interval[1];
+
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+
+        if (currEnd < newStart)
+        {
+            return -1;
+        }
+
+        if (currStart > newEnd)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+        vector<vector<int>> finalIntervals;
+        int n = intervals.size(), index = n;
+        bool doesAnyMergingIntervalsLeft = true;
+
+        for (int ind = 0; ind < n; ind++)
+        {
+            vector<int> &currInterval = intervals[ind];
+            int relationOfIntervals = isIntervalSmallerGreaterOrCanBeMerged(currInterval, newInterval);
+
+            switch (relationOfIntervals)
+            {
+            case -1:
+                finalIntervals.push_back(currInterval);
+                break;
+
+            case 0:
+                newInterval[0] = min(newInterval[0], currInterval[0]);
+                newInterval[1] = max(newInterval[1], currInterval[1]);
+                break;
+
+            case 1:
+                index = ind;
+                doesAnyMergingIntervalsLeft = false;
+            }
+
+            if (!doesAnyMergingIntervalsLeft)
+                break;
+        }
+
+        finalIntervals.push_back(newInterval);
+
+        while (index < n)
+        {
+            vector<int> &interval = intervals[index];
+            finalIntervals.push_back(interval);
+            index++;
+        }
+
+        return finalIntervals;
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
 
 int main()
 {
