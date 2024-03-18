9381ec626a3d7e3347e3c5369d739fb84e42ec2c
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-03-18 22:02:20 +0530

    Leetcode Daily on 18 Mar 2024

--- Leetcode Daily Problems/Minimum Number of Arrows to Burst Balloons.cpp
+++ Leetcode Daily Problems/Minimum Number of Arrows to Burst Balloons.cpp
@@ -2,19 +2,64 @@
 using namespace std;
 
 /*
-Approach --
+Approach -- First of all we will sort the given points array according to startBoundary . If any two or more
+ballons overlap for some region then shooting a arrow in this common region will burst all the ballons .
+Now we will keep the track of the prevInterval and whenver currInterval can be merged with prevInterval we
+will update prevInterval as the common portion between them . If next interval is also going to merge with
+first two for some common area then it must be overlapped with the prevInterval . If currInterval cannot be
+merged with prevInterval then currInterval will become prevInterval and we update the arrow count by 1 more .
+We only need to keep the track of just prevInterval due to the fact that if any interval is not overlapped with
+just prev one then it is sure that it couldn't be overlapped with intevals before prevInterval as intervals are
+sorted by startingBoundary . Finally return the count of the arrows required to burst all the ballons .
 */
 
 class Solution
 {
+protected:
+    bool canBeMerged(vector<int> &prevInterval, vector<int> &currInterval)
+    {
+        int prevEnd = prevInterval[1];
+        int currStart = currInterval[0];
+
+        return (prevEnd >= currStart);
+    }
+
 public:
     int findMinArrowShots(vector<vector<int>> &points)
     {
+        sort(points.begin(), points.end());
+        vector<int> &prevInterval = points[0];
+        int n = points.size(), countOfArrowShots = 1;
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            vector<int> &currInterval = points[ind];
+
+            if (canBeMerged(prevInterval, currInterval))
+            {
+                int prevIntervalStart = prevInterval[0];
+                int prevIntervalEnd = prevInterval[1];
+
+                int currIntervalStart = currInterval[0];
+                int currIntervalEnd = currInterval[1];
+
+                int newIntervalStart = max(prevIntervalStart, currIntervalStart);
+                int newIntervalEnd = min(prevIntervalEnd, currIntervalEnd);
+
+                prevInterval = {newIntervalStart, newIntervalEnd};
+                continue;
+            }
+
+            countOfArrowShots++;
+            prevInterval = currInterval;
+        }
+
+        return countOfArrowShots;
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(N)
+// SC : O(1)
 
 int main()
 {
