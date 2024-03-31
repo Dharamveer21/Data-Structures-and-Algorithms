032dc4179b81342b4c999d0fb3ff95993e497cc5
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-03-31 15:33:48 +0530

    Leetcode Problems

---
+++ Leetcode Daily Problems/Count Subarrays With Fixed Bounds.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Approach -- 
+*/
+
+class Solution
+{
+public:
+    long long countSubarrays(vector<int> &nums, int minK, int maxK)
+    {
+    }
+};
+
+// TC : O()
+// SC : O()
+
+int main()
+{
+
+    return 0;
+}
--- Leetcode Daily Problems/Subarrays with K Different Integers.cpp
+++ Leetcode Daily Problems/Subarrays with K Different Integers.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 /*
-Approach -- Here we can use a standard sliding window template but it will give us the subarrays count ending at
-right pointer having unique elements <= k . Now , we can apply this sliding window again for <= k-1 . Finally we
-can subtract subarraysCount (for unique <= k-1) from subarraysCount (for unique <= k) . It will be (for == k) .
+Approach 1 -- Here we can use a standard sliding window template but it will give us the subarrays count ending
+at right pointer having unique elements <= k . Now , we can apply this sliding window again for <= k-1 .Finally
+we can subtract subarraysCount (for unique <= k-1) from subarraysCount (for unique <= k) . It will (for == k) .
+
+Approach 2 -- We just need to find the smallest valid subarray ending at right pointer and the pointer to the
+left of the largest valid subarray ending at right pointer . We just add (left Pointer - pointer to left of
+largest subarray + 1) for finding all valid subarrays ending at right pointer . We can find it for every right
+pointer using the sliding window technique . Finally return the ans for all valid subarrays (distinct == k) .
 */
 
-// Approach
+// Approach 1
 class Solution
 {
 protected:
@@ -51,7 +56,50 @@ public:
 };
 
 // TC : O(N)
-// SC : O(1)
+// SC : O(N)
+
+// Approach 2
+class Solution
+{
+public:
+    int subarraysWithKDistinct(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freqMap;
+        int maxUniqueElements1 = k, subarraysCountForExactlyKDistinctElements = 0;
+        int n = nums.size(), lastLeftPointer = 0, leftPointer = 0, rightPointer = 0;
+
+        while (rightPointer < n)
+        {
+            int num = nums[rightPointer++];
+            freqMap[num] += 1;
+
+            while (freqMap.size() > k)
+            {
+                int leftMostElement = nums[leftPointer++];
+                freqMap[leftMostElement] -= 1;
+
+                if (freqMap[leftMostElement] == 0)
+                    freqMap.erase(leftMostElement);
+
+                lastLeftPointer = leftPointer;
+            }
+
+            while (freqMap[nums[leftPointer]] > 1)
+                freqMap[nums[leftPointer++]] -= 1;
+
+            if (freqMap.size() == k)
+            {
+                int validSubarraysCountEndingAtRightPointer = leftPointer - lastLeftPointer + 1;
+                subarraysCountForExactlyKDistinctElements += validSubarraysCountEndingAtRightPointer;
+            }
+        }
+
+        return subarraysCountForExactlyKDistinctElements;
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
 
 int main()
 {
