37b968908645ded3a111a050d1a9fe157db11e9f
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-04-16 18:36:03 +0530

    Leetcode Problems

--- Leetcode Daily Problems/Time Needed to Buy Tickets.cpp
+++ Leetcode Daily Problems/Time Needed to Buy Tickets.cpp
@@ -1,17 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//
+// Sum of minimum of time of a person and kth person
 class Solution
 {
 public:
     int timeRequiredToBuy(vector<int> &tickets, int k)
     {
+        int numberOfTickets = tickets.size();
+        int requiredPerson = k, requiredTime = 0;
+
+        for (int person = 0; person < numberOfTickets; person++)
+        {
+            int timeTakenByCurrenPerson = tickets[person];
+            int timeTakenByRequiredPerson = tickets[requiredPerson];
+
+            if (person <= requiredPerson)
+                requiredTime += min(timeTakenByCurrenPerson, timeTakenByRequiredPerson);
+
+            else
+                requiredTime += min(timeTakenByCurrenPerson, timeTakenByRequiredPerson - 1);
+        }
+
+        return requiredTime;
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(N)
+// SC : O(1)
 
 int main()
 {
