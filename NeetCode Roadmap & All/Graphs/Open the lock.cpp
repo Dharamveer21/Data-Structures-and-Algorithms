4034f9f2f1e03bdedba772debf87cfb59ec13950
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-04-24 22:04:11 +0530

    Leetcode Problems

--- NeetCode Roadmap & All/Graphs/Open the lock.cpp
+++ NeetCode Roadmap & All/Graphs/Open the lock.cpp
@@ -4,15 +4,51 @@ using namespace std;
 // Breadth First Search
 class Solution
 {
+protected:
+    void fillTheNextCombinations(string &currCombination, queue<string> &queueOfAllCombinations, unordered_set<string> &visitedCombination)
+    {
+        for (int pos = 0; pos < 4; pos++)
+        {
+            char ch = currCombination[pos];
+
+            char decreaseChar = ((ch == '0') ? '9' : ch - 1);
+            char increaseChar = ((ch == '9') ? '0' : ch + 1);
+
+            currCombination[pos] = decreaseChar;
+
+            if (visitedCombination.find(currCombination) == visitedCombination.end())
+            {
+                queueOfAllCombinations.push(currCombination);
+                visitedCombination.insert(currCombination);
+            }
+
+            currCombination[pos] = increaseChar;
+
+            if (visitedCombination.find(currCombination) == visitedCombination.end())
+            {
+                queueOfAllCombinations.push(currCombination);
+                visitedCombination.insert(currCombination);
+            }
+
+            currCombination[pos] = ch;
+        }
+    }
+
 public:
     int openLock(vector<string> &deadends, string target)
     {
-        int minimumTurnsToOpenLock = 0;
-        queue<string> queueOfAllCombinations;
-        unordered_map<string, bool> visitedCombination;
+        int minimumTurnsToOpenLock = -1;
+        string startCombination = "0000";
 
-        queueOfAllCombinations.push("0000");
-        visitedCombination["0000"] = true;
+        queue<string> queueOfAllCombinations;
+        queueOfAllCombinations.push(startCombination);
+
+        unordered_set<string> visitedCombination(deadends.begin(), deadends.end());
+
+        if (visitedCombination.find(startCombination) != visitedCombination.end())
+            return minimumTurnsToOpenLock;
+
+        visitedCombination.insert(startCombination);
 
         while (!queueOfAllCombinations.empty())
         {
@@ -21,15 +57,26 @@ public:
 
             while (combinationsCount--)
             {
+                string currCombination = queueOfAllCombinations.front();
+                queueOfAllCombinations.pop();
+
+                if (currCombination == target)
+                    return minimumTurnsToOpenLock;
+
+                fillTheNextCombinations(currCombination, queueOfAllCombinations, visitedCombination);
             }
         }
 
+        minimumTurnsToOpenLock = -1;
         return minimumTurnsToOpenLock;
     }
 };
 
-// TC : O()
-// SC : O()
+// N -- number of positions = 10 (0 To 9)
+// W -- number of wheels = 4 ("0000")
+
+// TC : O(N ^ W)
+// SC : O(N ^ W)
 
 int main()
 {
