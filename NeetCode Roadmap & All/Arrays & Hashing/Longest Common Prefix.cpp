680f5ab2cb9aea4b9bbdc7de6b6dbd52cd0536b9
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-05-04 16:57:08 +0530

    Longest Common Prefix

--- NeetCode Roadmap & All/Arrays & Hashing/Longest Common Prefix.cpp
+++ NeetCode Roadmap & All/Arrays & Hashing/Longest Common Prefix.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sorting
 class Solution
 {
 public:
     string longestCommonPrefix(vector<string> &strs)
     {
+        int numStrings = strs.size();
+        sort(strs.begin(), strs.end());
+        string longestCommonPrefixInStrings;
+
+        string &firstString = strs[0], &lastString = strs[numStrings - 1];
+        int len1 = firstString.length(), len2 = lastString.length();
+
+        for (int ind = 0; ind < min(len1, len2); ind++)
+        {
+            char ch1 = firstString[ind];
+            int ch2 = lastString[ind];
+
+            if (ch1 != ch2)
+                break;
+
+            longestCommonPrefixInStrings.push_back(ch1);
+        }
+
+        return longestCommonPrefixInStrings;
     }
 };
+
+// TC : O(N * log(N))
+// SC : O(1)
 
 int main()
 {
