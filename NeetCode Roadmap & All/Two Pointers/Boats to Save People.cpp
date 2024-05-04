184799b0a3d07154b9277200d5cf8756291c21ee
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-05-04 16:33:36 +0530

    Boats to Save People

--- NeetCode Roadmap & All/Two Pointers/Boats to Save People.cpp
+++ NeetCode Roadmap & All/Two Pointers/Boats to Save People.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// Two Pointers
 class Solution
 {
 public:
     int numRescueBoats(vector<int> &people, int limit)
     {
+        sort(people.begin(), people.end());
+        int rightPointer = people.size() - 1;
+        int minNumOfBoats = 0, leftPointer = 0;
+
+        while (leftPointer <= rightPointer)
+        {
+            int lightWeight = people[leftPointer];
+            int heavyWeight = people[rightPointer];
+
+            if ((lightWeight + heavyWeight) <= limit)
+                leftPointer++;
+
+            rightPointer--;
+            minNumOfBoats++;
+        }
+
+        return minNumOfBoats;
     }
 };
 
-// TC : O()
-// SC : O()
+// TC : O(N)
+// SC : O(1)
 
 int main()
 {
