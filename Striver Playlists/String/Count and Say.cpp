e7efc9d4d5d2c9bbde2128f4d63d68ee9132e5ed
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2024-06-17 14:26:01 +0000

    Count and Say Half done

--- Striver Playlists/String/Count and Say.cpp
+++ Striver Playlists/String/Count and Say.cpp
@@ -3,14 +3,46 @@ using namespace std;
 
 class Solution
 {
+protected:
+    string newCountAndSayString(string &str)
+    {
+        int n = str.length();
+        int first = 0,countOccurrence = 0;
+
+        for(int ind=0;ind<n;ind++)
+        {
+            int curr = str[ind];
+
+            if(curr != first)
+            {
+                
+
+                first = curr;
+                countOccurrence = 1;
+            }
+
+            else
+            {
+                countOccurrence++;
+            }
+        }
+    }
+
 public:
     string countAndSay(int n)
     {
+        if (n == 1)
+            return "1";
+
+        string prevStr = countAndSay(n-1);
+        string currStr = newCountAndSayString(prevStr);
+
+        return currStr;    
     }
 };
 
 int main()
 {
-
+    
     return 0;
 }
