da5c5772e5f0c23a7def9d53d6d1b4db2c24cff7
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-08-06 23:01:24 +0530

    Two String Problems Leetcode

--- Striver Playlists/String/Largest Odd Number in a String.cpp
+++ Striver Playlists/String/Largest Odd Number in a String.cpp
@@ -1,13 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// String & Implimentation
 class Solution
 {
 public:
-    string largestOddNumber(string num)
+    string largestOddNumber(string &num)
     {
+        int n = num.length(), len = 0;
+
+        for (int ind = n - 1; ind >= 0; ind--)
+        {
+            char ch = num[ind];
+            int digit = ch - '0';
+
+            if (digit & 1)
+            {
+                len = ind + 1;
+                break;
+            }
+        }
+
+        string largestOdd = num.substr(0, len);
+        return largestOdd;
     }
 };
+
+// TC : O(N)
+// SC : O(N)
 
 int main()
 {
--- Striver Playlists/String/Minimum Add to Make Parentheses Valid.cpp
+++ Striver Playlists/String/Minimum Add to Make Parentheses Valid.cpp
@@ -1,13 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Stack
 class Solution
 {
 public:
     int minAddToMakeValid(string s)
     {
+        stack<char> openParanthesis;
+        int n = s.length(), minMoves = 0;
+
+        for (int ind = 0; ind < n; ind++)
+        {
+            char ch = s[ind];
+
+            switch (ch)
+            {
+            case '(':
+                openParanthesis.push(ch);
+                break;
+
+            case ')':
+                if (openParanthesis.empty())
+                    minMoves++;
+
+                else
+                    openParanthesis.pop();
+            }
+        }
+
+        minMoves += openParanthesis.size();
+        return minMoves;
     }
 };
+
+// TC : O(N)
+// SC : O(N)
+
+// Logic & Implimentation
+class Solution
+{
+public:
+    int minAddToMakeValid(string s)
+    {
+        int openParanthesisCount = 0;
+        int n = s.length(), minMoves = 0;
+
+        for (int ind = 0; ind < n; ind++)
+        {
+            char ch = s[ind];
+
+            switch (ch)
+            {
+            case '(':
+                openParanthesisCount++;
+                break;
+
+            case ')':
+                if (openParanthesisCount == 0)
+                    minMoves++;
+
+                else
+                    openParanthesisCount--;
+            }
+        }
+
+        minMoves += openParanthesisCount;
+        return minMoves;
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
 
 int main()
 {
