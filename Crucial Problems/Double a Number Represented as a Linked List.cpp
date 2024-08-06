2b44d0179688780e74a519860c2c0dfa5e54c69b
Dharamveer21 <chauhandharamveer21@gmail.com>
2024-08-06 22:01:13 +0530

    Count and Say

---
+++ Crucial Problems/Double a Number Represented as a Linked List.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next = NULL;
+
+    ListNode(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+//
+class Solution
+{
+public:
+    ListNode *doubleIt(ListNode *head)
+    {
+    }
+};
+
+// TC : O()
+// SC : O()
+
+int main()
+{
+    return 0;
+}
